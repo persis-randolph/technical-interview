773027a2005f0f1ab15563a7efe8d81db43b8a4b
persis-randolph <persis.randolph@gmail.com>
2023-06-28 12:54:11 -0500

    add config module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JobsModule } from './jobs/jobs.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [JobsModule],
+  imports: [JobsModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
